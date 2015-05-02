95739cdc90a0267fb0cff2434b1be9653609c571
Dickson Tam <dicksont650@gmail.com>
2015-05-02 11:15:45 -0700

    Module signature changed to function called with a single argument that should be set to window. Bumped version to 0.0.1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-(function() {
-  var gui = require('nw.gui');
+module.exports = function(window) {
+  var gui = window.require('nw.gui');
   if (process.platform === "darwin") {
     var mb = new gui.Menu({type: 'menubar'});
     mb.createMacBuiltin('My App');
     gui.Window.get().menu = mb;
   }
-})();
+};
